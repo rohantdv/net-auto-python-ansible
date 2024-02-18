1dea6a4eb5e8fb07ae56bd219d752382770977b3
rohantdv <rohan.tdv@gmail.com>
2024-02-18 16:04:24 +0800

    final

---
+++ l1netmiko.py
@@ -0,0 +1,28 @@
+from netmiko import ConnectHandler
+
+iosv_l2 = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.195.194',
+    'username': 'admin',
+    'password': 'cisco',
+}
+
+net_connect = ConnectHandler(**iosv_l2)
+output = net_connect.send_command('show ip int brief')
+print(output)
+
+config_commands = ['int loop 1', 'ip address 1.1.1.1 255.255.255.0']
+output = net_connect.send_config_set(config_commands)
+print(output)
+
+for n in range(2, 21):
+    print("Creating VLAN " + str(n))
+    config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
+    output = net_connect.send_config_set(config_commands)
+    print(output)
+
+output = net_connect.send_command('show ip int brief')
+print(output)
+
+output = net_connect.send_command('show vlan brief')
+print(output)
